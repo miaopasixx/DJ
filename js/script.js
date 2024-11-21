fb8f0ff06092d2f6c42995d248f15c8db5389e10
miaopasixx <a13026588708@163.com>
2024-11-21 16:11:39 +0800

    剔除下划线

--- js/script.js
+++ js/script.js
@@ -625,7 +625,7 @@ function displayFilesWithPagination(files) {
                         <div style="break-inside: avoid; margin-bottom: 10px;">
                             <img src="${fileURL}" loading="lazy" style="width: 100%; height: auto; border-radius: 5px;">
                             <a href="${fileURL}" download="${fileObj.name}" style="display: none;"></a>
-                            <h4 onclick="downloadFile('${fileURL}', '${fileObj.name}')" style="color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px; cursor: pointer;">${fileObj.name}</h4>
+                            <h4 onclick="downloadFile('${fileURL}', '${fileObj.name}')" style="color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px; cursor: pointer; text-decoration: none;">${fileObj.name}</h4>
                         </div>
                     `;
                 }).join('')}
@@ -655,7 +655,7 @@ function displayFilesWithPagination(files) {
                         <div style="position: relative; margin-bottom: 30px; width: calc(16.66% - 10px);">
                             <video src="${fileURL}" poster="${posterURL}" preload="none" style="width: 100%; height: auto; border-radius: 5px; cursor: pointer;" controls></video>
                             <a href="${fileURL}" download="${fileObj.name}" style="position: absolute; top: 5px; right: 5px; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px;">下载</a>
-                            <h4 style="position: absolute; bottom: -70px; left: 0px; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px;">${fileObj.name}</h4>
+                            <h4 style="position: absolute; bottom: -70px; left: 0px; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-decoration: none;">${fileObj.name}</h4>
                         </div>
                     `;
                 }).join('')}
@@ -680,7 +680,7 @@ function displayFilesWithPagination(files) {
                     return `
                         <div style="break-inside: avoid; margin-bottom: 10px;">
                             <iframe src="${fileURL}" style="width: 100%; height: 297px; border: none; margin-top: 5px;"></iframe>
-                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px;">${fileObj.name}</a>
+                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px; text-decoration: none;">${fileObj.name}</a>
                         </div>
                     `;
                 }).join('')}
@@ -714,7 +714,7 @@ function displayFilesWithPagination(files) {
                     return `
                         <div style="break-inside: avoid; margin-bottom: 10px;">
                             <div id="sheet-${fileObj.name}" style="width: 100%; height: 297px; border: none; margin-top: 5px; overflow: hidden;"></div>
-                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px;">${fileObj.name}</a>
+                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px; text-decoration: none;">${fileObj.name}</a>
                         </div>
                     `;
                 }).join('')}
@@ -737,7 +737,7 @@ function displayFilesWithPagination(files) {
                     return `
                         <div style="break-inside: avoid; margin-bottom: 10px;">
                             <iframe src="https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(fileURL)}" style="width: 100%; height: 297px; border: none; margin-top: 5px;"></iframe>
-                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px;">${fileObj.name}</a>
+                            <a href="${fileURL}" download="${fileObj.name}" style="display: block; color: white; background: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; text-align: center; margin-top: 5px; text-decoration: none;">${fileObj.name}</a>
                         </div>
                     `;
                 }).join('')}
